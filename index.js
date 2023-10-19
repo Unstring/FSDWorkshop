a1693b343cbdb5ae8f92c8dc6b90e4459ea3396b
Amit Anand <dropmail.amit@gmail.com>
2023-10-19 22:10:15 +0530

    base setup

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
-
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const cors = require("cors");
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
 const app = express();
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("database connected"))
+  .catch((err) => console.log(`error ${err}`));
 
-
+app.use(expressValidator());
 app.use(morgan("dev"));
 app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 app.get("/", (req, res) => {
@@ -26,7 +41,7 @@ app.use((err, req, res) => {
   }
 });
 
-const port =  3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`the  server is running on port ${port}`);
 });
